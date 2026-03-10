bd2981feb58748e690b2c04a365edba0256d3f6e
bibenga <{ID}+{username}@users.noreply.github.com>
2026-03-10 15:27:51 +0000

    update test

--- src/test/java/com/github/bibenga/alns/select/AbstractOperatorSelectionSchemeTest.java
+++ src/test/java/com/github/bibenga/alns/select/AbstractOperatorSelectionSchemeTest.java
@@ -14,42 +14,58 @@ import com.github.bibenga.alns.State;
 public class AbstractOperatorSelectionSchemeTest {
 
     @Test
-    void testValidation() {
-        new DummyOperatorSelection(2, 3, null);
+    void testValid() {
+        assertDoesNotThrow(() -> new DummyOperatorSelection(2, 3, null));
+    }
 
-        Exception ex = assertThrows(IllegalArgumentException.class,
+    @Test
+    void testNegativeDestroyCount() {
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
                 () -> new DummyOperatorSelection(-2, 3, null));
         assertEquals("Missing destroy or repair operators.", ex.getMessage());
     }
 
+    // --- testCouplingValidation ---
+
     @Test
-    void testCouplingValidation() {
-        Exception ex;
+    void testValicCoupling() {
+        new DummyOperatorSelection(2, 3,
+                new boolean[][] { { true, true, true }, { true, true, true } });
+    }
 
-        assertDoesNotThrow(() -> new DummyOperatorSelection(2, 3,
-                new boolean[][] { { true, true, true }, { true, true, true } }));
-
-        ex = assertThrows(IllegalArgumentException.class,
+    @Test
+    void testEmptyCoupling() {
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
                 () -> new DummyOperatorSelection(2, 3, new boolean[][] {}));
         assertEquals("Coupling matrix of shape (0, 0), expected (2, 3)", ex.getMessage());
+    }
 
-        ex = assertThrows(IllegalArgumentException.class,
-                () -> new DummyOperatorSelection(2, 3,
-                        new boolean[][] { { true, true }, { true } }));
-        assertEquals("The number of columns in a row 1 does not match the expected 2", ex.getMessage());
-
-        ex = assertThrows(IllegalArgumentException.class,
+    @Test
+    void testDifferentSizeCoupling() {
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
                 () -> new DummyOperatorSelection(2, 3,
                         new boolean[][] { { true, true }, { true, true }, { true, true } }));
         assertEquals("Coupling matrix of shape (3, 2), expected (2, 3)", ex.getMessage());
+    }
 
-        ex = assertThrows(IllegalArgumentException.class,
+    @Test
+    void testMissedCoupling() {
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
+                () -> new DummyOperatorSelection(2, 3,
+                        new boolean[][] { { true, true }, { true } }));
+        assertEquals("The number of columns in a row 1 does not match the expected 2",
+                ex.getMessage());
+    }
+
+    @Test
+    void testDestroyWithoutRepairCoupling() {
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
                 () -> new DummyOperatorSelection(2, 3,
                         new boolean[][] { { true, false, false }, { false, false, false } }));
         assertEquals("Destroy operator 1 has no coupled repair operators", ex.getMessage());
     }
 
-    class DummyOperatorSelection extends AbstractOperatorSelectionScheme {
+    static class DummyOperatorSelection extends AbstractOperatorSelectionScheme {
 
         protected DummyOperatorSelection(int numDestroy, int numRepair) {
             super(numDestroy, numRepair);
